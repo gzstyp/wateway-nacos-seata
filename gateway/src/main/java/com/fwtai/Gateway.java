80c80454c514a94b9e90339dbd50b494eb5b85d4
gzstyp <444141300@qq.com>
2021-03-02 18:43:46 +0800

    端口号

--- gateway/src/main/java/com/fwtai/Gateway.java
+++ gateway/src/main/java/com/fwtai/Gateway.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//本项目spring-cloud-gateway已整合nacos 访问 http://localhost:9000/nacos-consumer-feign/test/hi?token=value 说明 Spring Cloud Gateway 的路由功能配置成功
+//本项目spring-cloud-gateway已整合nacos 访问 http://localhost:9000/consumer/test/hi?token=value 说明 Spring Cloud Gateway 的路由功能配置成功
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
