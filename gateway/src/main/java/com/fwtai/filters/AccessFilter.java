facc4cf576ab617d96a4895ce65147cdf23cd1a5
gzstyp <444141300@qq.com>
2021-03-05 11:15:13 +0800

    鉴权过滤器,全局的路由网关

--- gateway/src/main/java/com/fwtai/filters/AccessFilter.java
+++ gateway/src/main/java/com/fwtai/filters/AccessFilter.java
@@ -1,59 +1,69 @@
 package com.fwtai.filters;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
-import java.util.Map;
 
 /**
  * 鉴权过滤器,全局的路由网关
  */
-@Component
+//@Component
 public class AccessFilter implements GlobalFilter,Ordered {
 
+    final Logger logger = LoggerFactory.getLogger(AccessFilter.class);
+
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        final String token = exchange.getRequest().getQueryParams().getFirst("token");
-        if (token == null || token.isEmpty()) {
-            ServerHttpResponse response = exchange.getResponse();
-            // 封装错误信息
-            final Map<String, Object> responseData = Maps.newHashMap();
-            responseData.put("code", 401);
-            responseData.put("message", "非法请求");
-            responseData.put("cause", "Token is empty");
-            try {
-                // 将信息转换为 JSON
-                final ObjectMapper objectMapper = new ObjectMapper();
-                final byte[] data = objectMapper.writeValueAsBytes(responseData);
-                // 输出错误信息到页面
-                final DataBuffer buffer = response.bufferFactory().wrap(data);
-                response.setStatusCode(HttpStatus.UNAUTHORIZED);
-                response.getHeaders().add("Content-Type","text/html;charset=UTF-8");
-                return response.writeWith(Mono.just(buffer));
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
+    public Mono<Void> filter(final ServerWebExchange exchange,final GatewayFilterChain chain){
+        final ServerHttpRequest request = exchange.getRequest();
+        final ServerHttpResponse response = exchange.getResponse();
+        final String uri = request.getURI().getPath();
+        if (request.getMethod() == HttpMethod.OPTIONS) {
+            response.setStatusCode(HttpStatus.OK);
+            return Mono.empty();
         }
-        return chain.filter(exchange);
+        final String token = exchange.getRequest().getQueryParams().getFirst("token");
+        if(token == null || token.isEmpty())
+            return reply401(response);
+        if(uri.contains("/user/login")){//放行
+            return chain.filter(exchange);//继续执行下一个过滤器
+        }
+        final String accessTokenUrl = request.getQueryParams().getFirst("accessToken");
+        final String accessTokenHeader = request.getHeaders().getFirst("accessToken");
+        if((accessTokenUrl != null && accessTokenUrl.length() > 0 ) || (accessTokenHeader != null && accessTokenHeader.length() > 0)){
+            System.out.println("鉴权|权限验证全局过滤器被执行,优先于全局过滤器,优先于网关过滤器,因为order的值越小优先权越大");
+            return chain.filter(exchange);//继续执行下一个过滤器
+        }else{
+            logger.warn("accessToken 为null哦");
+            return reply401(response);
+        }
     }
 
-    /**
-     * 设置过滤器的执行顺序
-     *
-     * @return
-     */
     @Override
-    public int getOrder() {
-        return Ordered.LOWEST_PRECEDENCE;
+    public int getOrder(){
+        return 0;//数字越小优先权越大
+    }
+
+    private Mono<Void> reply401(final ServerHttpResponse response){
+        response.getHeaders().add("Content-Type","text/html;charset=utf-8");
+        final String msg = "{\"code\":401,\"msg\":\"没有操作权限\"}";
+        final DataBuffer db = response.bufferFactory().wrap(msg.getBytes());
+        return response.writeWith(Mono.just(db));
+    }
+
+    //还可以重定向获取请求的资源
+    private Mono<Void> replyRedirect(final ServerHttpResponse response){
+        response.setStatusCode(HttpStatus.SEE_OTHER);
+        response.getHeaders().set(HttpHeaders.LOCATION,"http://yinlz.com");
+        return response.setComplete();
     }
 }
